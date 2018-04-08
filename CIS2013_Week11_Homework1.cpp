f117769e6258bba446e953cc75828d2e8d61f8a8
Arkhane Sagunasingha <arkhane@okstate.edu>
2018-04-07 22:28:06 -0500

    Initial commit

---
+++ CIS2013_Week11_Homework1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+
+class CarClass
+{
+private:
+	char color, make, model, year;
+	int currentSpeed = 0;
+
+public:
+
+
+	void Attributes()
+	{
+		
+	}
+
+
+};
+
+
+int main()
+{
+	CarClass myFirstCar;
+	myFirstCar.
+	return 0;
+}
